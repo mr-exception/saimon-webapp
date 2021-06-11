a5282ec74e699ced1f350eebc91252c0e1f899b8
mr-exception <darbandi1996@gmail.com>
2021-06-11 22:36:55 +0430

    worked on profile

--- src/containers/Profile/Profile.tsx
+++ src/containers/Profile/Profile.tsx
@@ -5,8 +5,10 @@ import { selectAppKey } from "redux/types/selectors";
 const Profile = () => {
   const app_key = useSelector(selectAppKey);
   return (
-    <div className="profile-container">
-      <div className="address-box">{app_key.getPublicKeyNormalized()}</div>
+    <div className="profile-container flex flex-row h-full justify-center items-center">
+      <div className="address-box bg-base border-2 border-secondary rounded-lg p-8 flex flex-row justify-center items-center">
+        <span className="w-full">{app_key.getPublicKeyNormalized()}</span>
+      </div>
     </div>
   );
 };
