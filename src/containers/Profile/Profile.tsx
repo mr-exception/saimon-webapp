5c00ba4ed13d093496668c3cda7af39a53ae289c
mr-exception <darbandi1996@gmail.com>
2021-06-24 11:12:18 +0430

    changed https ports in service

--- src/containers/Profile/Profile.tsx
+++ src/containers/Profile/Profile.tsx
@@ -2,10 +2,22 @@ import React from "react";
 import "./styles.css";
 import { useSelector } from "react-redux";
 import { selectAppKey } from "redux/types/selectors";
+/**
+ * profile features:
+ * edit the account information
+ * copy the address into clipboard
+ * change the address
+ * remove profile from device
+ */
 const Profile = () => {
   const app_key = useSelector(selectAppKey);
   return (
-    <div className="profile-container flex flex-row h-full justify-center items-center">
+    <div className="profile-container">
+      <div className="general-information">
+        <div className="first-name">
+          
+        </div>
+      </div>
       <div className="address-box bg-base border-2 border-secondary rounded-lg p-8 flex flex-row justify-center items-center">
         <span className="w-full">{app_key.getPublicKeyNormalized()}</span>
       </div>
