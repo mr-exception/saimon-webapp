cbece8765e8d0ef45bf3c2687ad9ac143279ed11
mr-exception <darbandi1996@gmail.com>
2021-07-03 16:54:16 +0430

    changed sw

--- src/index.tsx
+++ src/index.tsx
@@ -20,5 +20,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-// enable service worker only on production mode
 serviceWorker.register();
